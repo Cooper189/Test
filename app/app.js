d00bf2d4a0e8c89b654d332a6c556b183189edf7
Cooper189 <anton.bilousov@gmail.com>
2016-12-28 00:27:22 +0200

    refactoring

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('app', []);
 
 app.directive('testSkyWell', ['mainFactory', function (mainFactory) {
 	return {
